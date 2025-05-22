b572ee779027b1480f189240a6791ec05d9eb42f
KanekoFutaba <fkaneko88@gmail.com>
2025-05-22 15:34:11 +0900

    ヘッダーに画像を表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # 本番環境だけで制限を有効にする
+  allow_browser versions: :modern if Rails.env.production?
 end
